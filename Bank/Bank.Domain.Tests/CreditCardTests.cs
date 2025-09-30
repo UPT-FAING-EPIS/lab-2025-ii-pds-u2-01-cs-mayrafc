b06dc9fefb334b6c7ab9a6f02a3059c6bbe64858
mayrafc <mc2021072620@virtual.upt.pe>
2025-09-29 21:21:04 -0500

    sin actividades

---
+++ Bank/Bank.Domain.Tests/CreditCardTests.cs
@@ -0,0 +1,40 @@
+using Bank.Domain;
+using NUnit.Framework;
+
+namespace Bank.Domain.Tests
+{
+    public class CreditCardTests
+    {
+
+        [Test]
+        public void GivenCreditTypeSelected_WhenRequestCreditCard_ThenNewValidCreditCard()
+        {
+            string cardType = "MoneyBack";
+            ICreditCard? cardDetails = CreditCardFactory.GetCreditCard(cardType);
+            Assert.IsNotNull(cardDetails);
+            Assert.IsNotEmpty(cardDetails.GetCardType());
+            Assert.GreaterOrEqual(cardDetails.GetCreditLimit(), 0);
+            Assert.GreaterOrEqual(cardDetails.GetAnnualCharge(), 0);
+        }
+                [Test]
+        public void GivenCreditTypePlatinumChoosen_WhenRequestCreditCard_ThenNewValidCreditCard()
+        {
+            ICreditCard creditCard = new PlatinumFactoryMethod().CreateProduct();
+            Assert.IsNotNull(creditCard);
+            Assert.IsNotEmpty(creditCard.GetCardType());
+            Assert.GreaterOrEqual(creditCard.GetCreditLimit(), 0);
+            Assert.GreaterOrEqual(creditCard.GetAnnualCharge(), 0);
+        }
+
+        [Test]
+        public void GivenCreditTypeTitaniumChoosen_WhenRequestCreditCard_ThenNewValidCreditCard()
+        {
+            ICreditCard creditCard = new TitaniumFactoryMethod().CreateProduct();
+            Assert.IsNotNull(creditCard);
+            Assert.IsNotEmpty(creditCard.GetCardType());
+            Assert.AreEqual(creditCard.GetCardType(),"Titanium Edge");
+            Assert.GreaterOrEqual(creditCard.GetCreditLimit(), 0);
+            Assert.GreaterOrEqual(creditCard.GetAnnualCharge(), 0);
+        }
+    }
+}
